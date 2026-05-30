40575b8319219c743d591ba78ed6966ddebfed20
Dalvae <diego.alvarez.e@ug.uchile.cl>
2026-05-30 16:43:51 -0400

    Agregar usuario CLIENTE de ejemplo para probar la autorizacion por rol

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,8 @@ INSERT INTO usuario (username, password) VALUES ('admin', '$2a$10$Cj2BHcrRNBXBuW
 
 -- 3 le asignamos el rol al usuario
 INSERT INTO usuario_roles (usuario_id, rol_id) VALUES (1, 1);
+
+-- 4 usuario de ejemplo SIN rol GERENTE (rol CLIENTE), misma clave '12345'
+INSERT INTO rol (nombre) VALUES ('CLIENTE');
+INSERT INTO usuario (username, password) VALUES ('cliente', '$2a$10$Cj2BHcrRNBXBuWWOcpSpWebu5mSiq5mnDiGSraHZwfxiAIlK8CR6i');
+INSERT INTO usuario_roles (usuario_id, rol_id) VALUES (2, 2);
